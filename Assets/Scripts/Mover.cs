34f27a298c1f41a9ea24ffe17a37f4d86ee0e389
Nikolay Semenov <164047031+iKabus@users.noreply.github.com>
2025-05-27 20:28:31 +0300

    FirstPassing

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,6 +5,7 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Vector3 _position;
     [SerializeField] private float _duraction;
+    
     private void Start()
     {
         transform.DOMove(_position, _duraction);
